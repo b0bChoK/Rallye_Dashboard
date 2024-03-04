c26afc4f11fd4a566e5ae619fd27dc0aed5a444a
DEWAS Albert <b0b_ChoK@yahoo.fr>
2024-03-04 23:12:56 +0100

    DOC: bump version and update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.b0bchok.rallye_dashboard_kt"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
