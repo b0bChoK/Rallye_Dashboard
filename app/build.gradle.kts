d1cdbd8eab7ca0b414c6374db42bcffa9d98be9b
DEWAS Albert <b0b_ChoK@yahoo.fr>
2024-04-15 19:12:04 +0200

    DOC: bump to 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.b0bchok.rallye_dashboard_kt"
         minSdk = 28
         targetSdk = 33
-        versionCode = 9
-        versionName = "1.7"
+        versionCode = 10
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
