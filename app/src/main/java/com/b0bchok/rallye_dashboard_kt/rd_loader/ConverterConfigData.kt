8a913a7b638616d80a56bcde91f59165ab67f8d9
DEWAS Albert <b0b_ChoK@yahoo.fr>
2024-09-25 20:40:31 +0200

    Update license to exclude rb_loader package from the GPL v3

--- app/src/main/java/com/b0bchok/rallye_dashboard_kt/rd_loader/ConverterConfigData.kt
+++ app/src/main/java/com/b0bchok/rallye_dashboard_kt/rd_loader/ConverterConfigData.kt
@@ -1,3 +1,28 @@
+/*
+Copyright (c) 2024 DEWAS Albert
+
+This license replace the general license of the software
+and apply to all file in this directory "rb_loader".
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this package and associated documentation files (the "package"),
+to deal in the package without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom
+the package is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall
+be included in all copies or substantial portions of the package.
+
+THE package IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE package.
+ */
+
 package com.b0bchok.rallye_dashboard_kt.rd_loader
 
 data class ConverterConfigData(
